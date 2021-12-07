6c5efac72f168e288219792f8ecfa13bfc21ed7d
Munir <munir2java@gmail.com>
2021-12-07 22:54:44 +0530

    keywordsandexpressions

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String []args){
+        //Expressions
+        // these are the building blocks of java prg..
+        //variable, value , operator and method calls r exprssions
+
+        int x = 10;//variable , value, operator
+        System.out.println("This is the system generated msg:");//method call
+
+        //converting miles to kilometres
+        //one mile equlas to 1.609433
+        //convert 100 miles to kms.
+
+        double kilometres = 100 * 1.609433 ;//
+
+        System.out.println("Here are the keywords and expressions");
+        System.out.println(kilometres);
+
+
+        //find our the statemnets and expressions below.
+
+        int score = 100;
+        if (score > 9){
+            System.out.println("U got the high score");
+            score = 0;
+        }
+
+    }
+}
